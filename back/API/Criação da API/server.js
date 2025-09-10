2959f67dfc69941b705c61c2aa9ae69de75377d9
MicaelTR <micaeltozir@gmail.com>
2025-09-10 16:28:34 -0300

    10/09/2025

--- back/API/Criação da API/server.js
+++ back/API/Criação da API/server.js
@@ -7,28 +7,61 @@ app.use(cors());
 
 const PORT = 3001;
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
 });
 
 let usuarios = [
-    {id: 1, nome: "Micael", idade: 16},
-    {id: 1, nome: "Micael", idade: 16},
-    {id: 1, nome: "Micael", idade: 16},
-    {id: 1, nome: "Micael", idade: 16}
+    { id: 1, nome: "Micael", idade: 16 },
+    { id: 1, nome: "Micael", idade: 16 },
+    { id: 1, nome: "Micael", idade: 16 },
+    { id: 1, nome: "Micael", idade: 16 }
 ]
 
-app.get("/", (require , response) => {
+app.get("/", (require, response) => {
     response.send("Testando a API")
 })
 
-app.get("/usuarios/:id", (require , response) => {
+app.get("/usuarios/:id", (require, response) => {
     const id = require.params.id
     const usuario = usuarios.find(user => user.id == id)
 
-    response.json(usuario)
+    if (usuario) {
+        response.json(usuario)
+    }
+    else {
+        response.status(404).json({ mensagem: "Usuário não encontrado" });
+    }
+});
+
+app.get("/usuarios/nome/:nome", (require, response) => {
+    const buscarNome = require.params.nome.toLowerCase()
+    const resultado = usuarios.filter(user => user.nome.toLowerCase().includes(buscarNome))
+
+    if (resultado.length > 0) {
+        response.json(resultado)
+    }
+    else {
+        response.status(404).json({ mensagem: "Nenhum usuário encontrado" });
+    }
 })
 
-app.get("/usuarios", (require , response) => {
+app.get("/usuarios", (require, response) => {
     response.json(usuarios)
+})
+
+app.delete("/usuarios/:id", (require, response) => {
+    const id = require.params.id
+    usuarios = usuarios.filter(user => user.id != id)
+    response.json({ mensagem: "Usuário removido com sucesso" })
+})
+
+app.post("/usuarios", (require, response) => {
+    const novoUsuario = {
+        id: usuarios.length + 1,
+        nome: require.body.nome,
+        idade: require.body.idade
+    };
+    usuarios.push(novoUsuario)
+    response.status(201).json(novoUsuario)
 })
