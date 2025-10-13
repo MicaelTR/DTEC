2c1a4393c413a27fcf1895c1ff1f3004969c8e6a
MicaelTR <micaeltozir@gmail.com>
2025-10-13 16:29:16 -0300

    13/10/2025

---
+++ front/site/REACT/Techboard/src/assets/Componetes/Tema/index.jsx
@@ -0,0 +1,10 @@
+import "../Tema/Tema.css"
+
+export function Tema() {
+  return (
+    <div className="tema">
+      <h2>Tema do Evento</h2>
+      <input type="text" placeholder="Tema do Evento" />
+    </div>
+  )
+}
